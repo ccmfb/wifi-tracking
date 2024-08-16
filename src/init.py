e492cfd571c3dabea7bbf9d1db2fbd4487b88f46
Carl <carl.c.b@icloud.com>
2024-08-16 14:16:54 +0100

    something happens, not sure if it works as supposed to

---
+++ src/init.py
@@ -0,0 +1,83 @@
+import os
+import pickle
+import json
+import yaml
+
+from tqdm import tqdm
+from shapely.geometry import Polygon
+from shapely.strtree import STRtree
+
+
+
+floor_trees = {}
+room_geometries = {}
+
+
+# Data from floors by id
+path = '../data/floorplans-main/floors_by_id'
+
+all_entries = os.listdir(path)
+floor_ids = [entry for entry in all_entries if os.path.isdir(os.path.join(path, entry))]
+
+
+for floor_id in tqdm(floor_ids):
+
+    workspace_path = f"{path}/{floor_id}/workspace.json"
+    info_path = f"{path}/{floor_id}/info.json"
+
+    with open(workspace_path, 'r') as file:
+        workspace_data = json.load(file)
+
+    with open(info_path, 'r') as file:
+        info_data = json.load(file)
+
+    building_id = info_data['buildingId']
+    offset_path = f"../data/floorplans-main/buildings_by_id/{building_id}/state.yaml"
+    if not os.path.exists(offset_path):
+        continue
+
+    with open(offset_path) as file:
+        offset_data = yaml.safe_load(file)
+        # check if offset is present
+        if offset_data['layout'] is None:
+            continue
+        viewbox = [float(num) for num in offset_data['layout']['viewbox'].split()]
+        offset = [-viewbox[0], -viewbox[1]]
+
+    current_polygons = []
+    for room in workspace_data:
+        room_id = room['id']
+        
+        room_coords = []
+        for coord in room['outline']['coords']:
+            room_coords.append((coord['x'] + offset[0], coord['y'] + offset[1])) 
+        
+        room_polygon = Polygon(room_coords)
+        room_geometries[room_id] = room_polygon
+        current_polygons.append(room_polygon)
+
+    floor_trees[floor_id] = STRtree(current_polygons)
+
+with open('../data/objects/floor_trees.pkl', 'wb') as file:
+    pickle.dump(floor_trees, file)
+
+with open('../data/objects/room_geometries.pkl', 'wb') as file:
+    pickle.dump(room_geometries, file)
+
+# ----------------------------------------------------------------------------------------------------------------------
+
+floorId_to_roomIds = {}
+
+
+for floor_id in tqdm(floor_ids):
+
+    workspace_path = f"{path}/{floor_id}/workspace.json"
+    with open(workspace_path, 'r') as file:
+        workspace_data = json.load(file)
+
+    room_ids = [room['id'] for room in workspace_data]
+
+    floorId_to_roomIds[floor_id] = room_ids
+
+with open('../data/id_mappings/floorId_to_roomIds.json', 'w') as file:
+    json.dump(floorId_to_roomIds, file)
--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+import csv
+import json
+import pickle
+import time
+
 from device import Device
 
+import pandas as pd
+import numpy as np
 from shapely.geometry import Polygon, Point
 from shapely.strtree import STRtree
 
@@ -8,28 +15,53 @@ ACTIVE_TIME = 3*60 # If devices is not seen for 3 minutes, it is considered inac
 ACTIVE_COUNT = 2 # If device is not at least seen 2 times, it is considered inactive -> probably only passing through
 
 
-def generate_refined_data(batch):
-    mapId_to_floorId = {} # Load from json
-    zValue_to_pValue = {} # Load from json
+def generate_refined_data(batch, first_batch=False):
+    # reset index of batch
+    batch = batch.reset_index(drop=True)
 
-    recent_devices = {} # Load from .pkl, format is {mac: Device}
+    #   _start = time.time()
+
+    with open('../data/id_mappings/floorId_to_mapId.json', 'r') as file:
+        floorId_to_mapId = json.load(file)
+        mapId_to_floorId = {v: k for k, v in floorId_to_mapId.items()}
+
+    with open('../data/zValue_to_pValue.json', 'r') as file:
+        zValue_to_pValue = json.load(file)
+        zValue_to_pValue = {float(k): v for k, v in zValue_to_pValue.items()}
+
+    if first_batch:
+        recent_devices = {} # Load from .pkl, format is {mac: Device}
+    else:
+        with open('../data/objects/recent_devices.pkl', 'rb') as file:
+            recent_devices = pickle.load(file)
+
     devices_in_batch = {}
 
-    floorId_to_roomIds = {} # Load from json
-    floor_str_trees = {} # Load from .pkl
+    with open('../data/id_mappings/floorId_to_roomIds.json', 'r') as file:
+        floorId_to_roomIds = json.load(file)
 
-    rooms_geometries = {}
-    floor_trees = {}
+    with open('../data/objects/room_geometries.pkl', 'rb') as file:
+        room_geometries = pickle.load(file)
+
+    with open('../data/objects/floor_trees.pkl', 'rb') as file:
+        floor_trees = pickle.load(file)
+
+
+    #   _stop = time.time()
+    #   print(f'Loading data took {_stop - _start:.2f} seconds')
+    #   _start = time.time()
 
 
     # Data to collect
-    timestamp = batch['timestamp'].to_list()[0]
+    timestamps = batch['timestamp'].to_list()
+    timestamp = timestamps[0]
     data_batch_timestamp = [timestamp for _ in range(len(batch))]
     data_mac = []
     data_x, data_y = [], []
     data_rssi = []
     data_floor_id = []
     data_room_id = []
+
 
     # Loading devices in current batch
     for i in range(len(batch)):
@@ -38,7 +70,11 @@ def generate_refined_data(batch):
             recent_devices[batch['mac'][i]] = Device(batch['mac'][i])
 
         current_device = recent_devices[batch['mac'][i]]
-        floor_id = mapId_to_floorId[batch['site_id'][i]]
+        if batch['map_id'][i] not in mapId_to_floorId:
+            print(f'Floor not found for mapId {batch["map_id"][i]}')
+            continue
+
+        floor_id = mapId_to_floorId[batch['map_id'][i]]
         current_device.add_data(
             batch['x'][i],
             batch['y'][i],
@@ -51,9 +87,10 @@ def generate_refined_data(batch):
             devices_in_batch[current_device.mac] = current_device
 
 
+
     for device in devices_in_batch.values():
         
-        if device.timestamp[-1] < timestamp - ACTIVE_TIME:
+        if device.timestamps[-1] < timestamps[-1] - ACTIVE_TIME:
             continue
 
         if len(device.positions) < ACTIVE_COUNT:
@@ -65,37 +102,56 @@ def generate_refined_data(batch):
         device.update_position(zValue_to_pValue)
 
         floor_id = device.floor_ids[-1]
-        floor_tree = floor_str_trees[floor_id]
-        room_ids = floorId_to_roomIds[floor_id]
+        floor_tree = floor_trees[floor_id]
+        room_ids = np.array(floorId_to_roomIds[floor_id])
 
         point = Point(device.x, device.y)
         matches = room_ids.take(floor_tree.query(point))
 
         room_id = 'None'
         for room_id_ in matches:
-            room = floor_id_to_roomIds[floor_id][room_id_]
+            room_geometry = room_geometries[room_id_]
 
-            if room.polygon.contains(point):
+            if room_geometry.contains(point):
                 room_id = room_id_
                 break
 
+        data_mac.append(device.mac)
+        data_x.append(device.x)
+        data_y.append(device.y)
+        data_rssi.append(device.rssi_values[-1])
+        data_floor_id.append(floor_id)
+        data_room_id.append(room_id)
 
+    #   _stop = time.time()
+    #   print(f'Processing data took {_stop - _start:.2f} seconds')
+    #   _start = time.time()
 
+    data = [
+        data_batch_timestamp,
+        data_mac,
+        data_x,
+        data_y,
+        data_rssi,
+        data_floor_id,
+        data_room_id
+    ]
+    transposed_data = list(map(list, zip(*data)))
 
+    with open('../data/data_refined.csv', 'a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerows(transposed_data)
 
+    # Updating recent devices
+    recent_devices.update(devices_in_batch)
+    new_recent_devices = {}
 
+    for mac, device in recent_devices.items():
+        if device.timestamps[-1] > timestamps[-1] - 60*20:
+            new_recent_devices[mac] = device
 
+    with open('../data/objects/recent_devices.pkl', 'wb') as file:
+        pickle.dump(new_recent_devices, file)
 
-        point = Point(device.x, device.y)
-        possible_room_ids = self.room_ids.take(self.str_tree.query(point))
-
-        actual_room_id = 'None'
-        for room_id in possible_room_ids:
-            room = self.rooms[self.room_ids.tolist().index(room_id)]
-
-            if room.polygon.contains(point):
-                actual_room_id = room_id
-
-
-
-
+    #   _stop = time.time()
+    #   print(f'Writing data took {_stop - _start:.2f} seconds')
